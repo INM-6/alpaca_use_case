295e542e352d1573b37334aa318dc7cb10e5e8d9
Cristiano Köhler <c.koehler@fz-juelich.de>
2024-02-06 20:10:42 +0100

    Updated environment scripts

--- environment/build_envs.sh
+++ environment/build_envs.sh
@@ -1,2 +1,4 @@
 conda env create --force -f no_provenance.yaml
 conda env create --force -f provenance.yaml
+conda env create --force -f provenance_mpi.yaml
+conda env create --force -f provenance_snakemake.yaml
